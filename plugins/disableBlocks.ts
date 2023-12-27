648b983c45e0c8576781a4679392f37254886351
adskyfly <466703496@qq.com>
2023-12-27 17:54:53 +0800

    bug fix

--- plugins/disableBlocks.ts
+++ plugins/disableBlocks.ts
@@ -1,6 +1,6 @@
 export default function plugin() {
   const transform = (code: string, id: string) => {
-    if (id.includes('pages/api/generate.ts')) {
+    if (id.includes('pages/api/generate.ts') || id.includes('pages/api/generate_chatgpt.ts')) {
       return {
         code: code.replace(/^.*?#vercel-disable-blocks([\s\S]+?)#vercel-end.*$/gm, ''),
         map: null,
